4a566dc4766394d51cf321356a56750873671486
Adele Lopez <adele@pachyderm.io>
2020-01-08 16:51:44 -0800

    put spout markers in special branch

--- src/server/pachyderm_test.go
+++ src/server/pachyderm_test.go
@@ -9649,6 +9649,7 @@ func TestSpout(t *testing.T) {
 
 		// create a spout pipeline
 		pipeline := "pipelinespoutmarker"
+
 		_, err := c.PpsAPIClient.CreatePipeline(
 			c.Ctx(),
 			&pps.CreatePipelineRequest{
@@ -9656,25 +9657,25 @@ func TestSpout(t *testing.T) {
 				Transform: &pps.Transform{
 					Cmd: []string{"/bin/sh"},
 					Stdin: []string{
-						"cp /pfs/marker/test ./test",
+						"cp /pfs/mymark/test ./test",
 						"while [ : ]",
 						"do",
 						"sleep 1",
 						"echo $(tail -1 test)x >> test",
-						"mkdir marker",
-						"cp test marker/test",
-						"tar -cvf /pfs/out ./marker/test*",
+						"mkdir mymark",
+						"cp test mymark/test",
+						"tar -cvf /pfs/out ./mymark/test*",
 						"done"},
 				},
 				Spout: &pps.Spout{
-					Marker: "marker",
+					Marker: "mymark",
 				},
 			})
 		require.NoError(t, err)
 
 		// get 5 succesive commits, and ensure that the file size increases each time
 		// since the spout should be appending to that file on each commit
-		iter, err := c.SubscribeCommit(pipeline, "master", nil, "", pfs.CommitState_FINISHED)
+		iter, err := c.SubscribeCommit(pipeline, "marker", nil, "", pfs.CommitState_FINISHED)
 		require.NoError(t, err)
 
 		var prevLength uint64
@@ -9699,18 +9700,18 @@ func TestSpout(t *testing.T) {
 				Transform: &pps.Transform{
 					Cmd: []string{"/bin/sh"},
 					Stdin: []string{
-						"cp /pfs/marker/test ./test",
+						"cp /pfs/mymark/test ./test",
 						"while [ : ]",
 						"do",
 						"sleep 1",
 						"echo $(tail -1 test). >> test",
-						"mkdir marker",
-						"cp test marker/test",
-						"tar -cvf /pfs/out ./marker/test*",
+						"mkdir mymark",
+						"cp test mymark/test",
+						"tar -cvf /pfs/out ./mymark/test*",
 						"done"},
 				},
 				Spout: &pps.Spout{
-					Marker: "marker",
+					Marker: "mymark",
 				},
 				Update: true,
 			})
@@ -9736,7 +9737,7 @@ func TestSpout(t *testing.T) {
 		// xxxxx....
 		// xxxxx.....
 		var buf bytes.Buffer
-		err = c.GetFile(pipeline, "master", "marker/test", 0, 0, &buf)
+		err = c.GetFile(pipeline, "marker", "mymark/test", 0, 0, &buf)
 		if err != nil {
 			t.Errorf("Could not get file %v", err)
 		}
@@ -9761,18 +9762,18 @@ func TestSpout(t *testing.T) {
 				Transform: &pps.Transform{
 					Cmd: []string{"/bin/sh"},
 					Stdin: []string{
-						"cp /pfs/marker/test ./test",
+						"cp /pfs/mymark/test ./test",
 						"while [ : ]",
 						"do",
 						"sleep 1",
 						"echo $(tail -1 test). >> test",
-						"mkdir marker",
-						"cp test marker/test",
-						"tar -cvf /pfs/out ./marker/test*",
+						"mkdir mymark",
+						"cp test mymark/test",
+						"tar -cvf /pfs/out ./mymark/test*",
 						"done"},
 				},
 				Spout: &pps.Spout{
-					Marker: "marker",
+					Marker: "mymark",
 				},
 				Update:    true,
 				Reprocess: true,
@@ -9786,7 +9787,7 @@ func TestSpout(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, 1, len(files))
 
-		err = c.GetFile(pipeline, "master", "marker/test", 0, 0, &buf)
+		err = c.GetFile(pipeline, "marker", "mymark/test", 0, 0, &buf)
 		if err != nil {
 			t.Errorf("Could not get file %v", err)
 		}
